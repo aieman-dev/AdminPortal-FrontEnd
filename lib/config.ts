5d18e73a11ed76b8d4d25c418850c3125421cfff
Aieman Hakimi <aiemanhakimi15@gmail.com>
2026-01-26 10:26:42 +0800

    add new backend api

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,3 @@
 
 // ngrok Url Server to connect to backend
-export const BACKEND_API_BASE = " https://hazel-nonpungent-yun.ngrok-free.dev";
+export const BACKEND_API_BASE = process.env.NEXT_PUBLIC_BACKEND_API_URL || "http://10.110.212.188:5000";
